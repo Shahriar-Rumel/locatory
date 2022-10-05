4aa1e64843a08aff69f0f2875fdd55aab6cf838c
fahmidareem3 <fahmidareem3@gmail.com>
2022-10-05 15:55:56 +0600

    Geocoding added for address

--- backend/models/Place.js
+++ backend/models/Place.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const geocoder = require("../utils/geocoder");
 
 const PlaceSchema = new mongoose.Schema({
   name: {
@@ -59,4 +60,22 @@ const PlaceSchema = new mongoose.Schema({
   },
 });
 
+// Geocode & create location field
+PlaceSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // // Do not save address in DB
+  // this.address = undefined;
+  next();
+});
 module.exports = mongoose.model("Place", PlaceSchema);
